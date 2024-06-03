6b8dc173003b2a0fb842d99b72cb0951604d0a49
silvertaqman <ajcedeno@espe.edu.ec>
2024-06-03 15:30:20 +0000

    data loaded is usable fit_params now works, errors while testing should be applied and corrected simulate_kinetics now is aboarded

--- R/fit_params.R
+++ R/fit_params.R
@@ -5,8 +5,8 @@
 #' @param method Selection of a linearization strategy ("lb" for Lineweaver-Burk, "eh" for Eadie-Hofstee, "hw" for Hanes-Woolf)
 #' @return A list with Km and Vmax parameters
 #' @examples
-#' fit_params(df, model="time", method="lb")
-fit_params <- function(df, model = c("time", "rate"), method = c("lin", "nonlin")) {
+#' fit_params(df, model="time", method="lin")
+fit_params <- function(df, model = c("time", "rate"), method = c("lin", "nonlin"),level=.95,...) {
   # Transform the data based on the model type
   if (model == "time") {
     # Set the appropriate column names for time (t) and substrate (S)
@@ -14,7 +14,7 @@ fit_params <- function(df, model = c("time", "rate"), method = c("lin", "nonlin"
     # Generates rate column and discard initial substrate
     df <- data.frame(
       S = df$S[-1],
-      v = diff(df$S) / diff(df$t)) #time2rate
+      v = - diff(df$S) / diff(df$t)) #time2rate
   } else if (model == "rate") {
     # Set the appropriate column names for substrate (S) and rate (v)
     colnames(df) <- c("S", "v")
@@ -33,23 +33,53 @@ fit_params <- function(df, model = c("time", "rate"), method = c("lin", "nonlin"
     eadie = v ~ I(v/S),
     hanes = I(S/v) ~ S)
   fits <- lapply(X = models, FUN = linfun, df=df)
-  params <- list(
-    lineweaver = ##### RETOMAR AQUI
-      # ORDENA LA LISTA DE CONSTANTES A REGRESAR
-    Km = as.double(stats::coef(fits[[1]])[2] / stats::coef(fits[[1]])[1]),
-    Vmax = as.double(1 / stats::coef(fits[[1]])[1]))
-  params <- list(
-    Km = as.double(-stats::coef(fits[[2]])[2]),
-    Vmax = as.double(stats::coef(fits[[2]])[1]))
-  params <- list(
-    Km = as.double(stats::coef(fits[[3]])[1] / stats::coef(fits[[3]])[2]),
-    Vmax = as.double(1 / stats::coef(3)[2]))
-  # return the params list
-  params
+  parms <- list(
+    lineweaver = list(
+      b = fits$lineweaver$coefficients[1],
+      m = fits$lineweaver$coefficients[2],
+      Km = fits$lineweaver$coefficients[2] / fits$lineweaver$coefficients[1],
+      Vmax = 1 / fits$lineweaver$coefficients[1]),
+    eadie = list(
+      b = fits$eadie$coefficients[1],
+      m = fits$eadie$coefficients[2],
+      Km = - fits$eadie$coefficients[2],
+      Vmax = fits$eadie$coefficients[1]),
+    hanes = list(
+      b = fits$hanes$coefficients[1],
+      m = fits$hanes$coefficients[2],
+      Km = fits$hanes$coefficients[1] / fits$hanes$coefficients[2],
+      Vmax = 1/fits$hanes$coefficients[2])
+  )
+  kinetics <- list(
+    lineweaver = fits$lineweaver |>
+      confint() |>
+      rbind(Vmax=c(parms$lineweaver$Vmax - qt(level, df=nrow(df)-2) * summary(fits$lineweaver)$coefficients[1,2] / parms$lineweaver$b^2,
+                   parms$lineweaver$Vmax + qt(level, df=nrow(df)-2) * summary(fits$lineweaver)$coefficients[1,2] / parms$lineweaver$b^2)) |>
+      rbind(Km=c(parms$lineweaver$Km - qt(level, df=nrow(df)-2) * sqrt(summary(fits$lineweaver)$coefficients[2,2]^2 /parms$lineweaver$b^2 + parms$lineweaver$m^2 * summary(fits$lineweaver)$coefficients[1,2]^2 / parms$lineweaver$b^4 - 2 * parms$lineweaver$m * vcov(fits$lineweaver)[2,1] / parms$lineweaver$b^3),
+                 parms$lineweaver$Km + qt(level, df=nrow(df)-2) *sqrt(summary(fits$lineweaver)$coefficients[2,2]^2 /parms$lineweaver$b^2 + parms$lineweaver$m^2 * summary(fits$lineweaver)$coefficients[1,2]^2 / parms$lineweaver$b^4 - 2 * parms$lineweaver$m * vcov(fits$lineweaver)[2,1] / parms$lineweaver$b^3))) |>
+      cbind(estimate=parms$lineweaver),
+    eadie = fits$eadie |>
+      confint(level=level,...) |>
+      rbind(Vmax=c(parms$eadie$Vmax - qt(level, df=nrow(df)-2) * summary(fits$eadie)$coefficients[1,2] / parms$eadie$b^2,
+                   parms$eadie$Vmax + qt(level, df=nrow(df)-2) * summary(fits$eadie)$coefficients[1,2] / parms$eadie$b^2)) |>
+      rbind(Km=c(parms$eadie$Km - qt(level, df=nrow(df)-2) * sqrt(summary(fits$eadie)$coefficients[2,2]^2 /parms$eadie$b^2 + parms$eadie$m^2 * summary(fits$eadie)$coefficients[1,2]^2 / parms$eadie$b^4 - 2 * parms$eadie$m * vcov(fits$eadie)[2,1] / parms$eadie$b^3),
+                 parms$eadie$Km + qt(level, df=nrow(df)-2) *sqrt(summary(fits$eadie)$coefficients[2,2]^2 /parms$eadie$b^2 + parms$eadie$m^2 * summary(fits$eadie)$coefficients[1,2]^2 / parms$eadie$b^4 - 2 * parms$eadie$m * vcov(fits$eadie)[2,1] / parms$eadie$b^3))) |>
+      cbind(estimate=parms$eadie),
+    hanes = fits$hanes |>
+      confint(level=level,...) |>
+      rbind(Vmax=c(parms$hanes$Vmax - qt(level, df=nrow(df)-2) * summary(fits$hanes)$coefficients[1,2] / parms$hanes$b^2,
+                   parms$hanes$Vmax + qt(level, df=nrow(df)-2) * summary(fits$hanes)$coefficients[1,2] / parms$hanes$b^2)) |>
+      rbind(Km=c(parms$hanes$Km - qt(level, df=nrow(df)-2) * sqrt(summary(fits$hanes)$coefficients[2,2]^2 /parms$hanes$b^2 + parms$hanes$m^2 * summary(fits$hanes)$coefficients[1,2]^2 / parms$hanes$b^4 - 2 * parms$hanes$m * vcov(fits$hanes)[2,1] / parms$hanes$b^3),
+                 parms$hanes$Km + qt(level, df=nrow(df)-2) *sqrt(summary(fits$hanes)$coefficients[2,2]^2 /parms$hanes$b^2 + parms$hanes$m^2 * summary(fits$hanes)$coefficients[1,2]^2 / parms$hanes$b^4 - 2 * parms$hanes$m * vcov(fits$hanes)[2,1] / parms$hanes$b^3)))  |>
+      cbind(estimate=parms$hanes)
+    )
+
+  # return the parameters list
+  return(kinetics)
 }
 
 # Example usage:
-# df <- datos |>
+# df <- kinetics |>
 # dplyr::filter(ecnumber == "4.1.1.39", type=="irreversible") |>
 # dplyr::select(time, substrate)
-# fit_params(df, model = "time", method = "lb")
+# fit_params(df, model = "time", method = "lin")
--- R/simulate_kinetics.R
+++ R/simulate_kinetics.R
@@ -1,36 +1,35 @@
-#' Use kinetic parameters to simulate data over time, substrate or rate
+#' Use kinetic parameters to simulate data over time, substrate or rate with Michaelis-Menten like models
 #'
 #' @param time A seq(a,b,n) vector setting size of simulation
 #' @param params list of parameters useful for the simulation (S0, Km, Vmax, Ki, Ka)
-#' @return df dataset with a time, substrate, reaction rate structure
+#' @param mode Choose between irreversible or reversible models
+#' @param inhibition Choose a kind of inhibition: competitive, uncompetitive and others
+#' @return df dataset with a time, substrate or reaction rate structure
 #' @examples
-#' simulate_kinetics(time, params=list(Km=0.0149,Vmax=2.533), inhibition=NULL)
-#Create an equations db
-# Definir las ecuaciones como funciones
-equations <- list(
-  # "irreversible MM rate equation"
-  A = function(time, params) {
-    df <- with(params, {
-      data.frame(
-        time=time,
-        Substrate = Km*pracma::lambertWp(S0*exp((S0-Vmax*time)/Km)/Km), #Schnell-Mendoza eq
-        rate = Vmax*Substrate/(Km+Substrate) # MM equation
-        )
-      }
-      )
-    return(df)},
-  # Inhibition
-  eq2 = function(time, params) { a * b * c },
-  eq3 = function(time, params) { a^2 + b^2 + c^2 }
-)
+#' simulate_kinetics(time, params=list(S0=1,Km=0.0149,Vmax=2.533), inhibition=NULL)
+# Create an equations db
 
-simulate_kinetics <- function(from=0, to=1, n=10, params, equations=equations) {
-  t = seq(from, to, n)
-  # Validar que params contiene los nombres correctos
+simulate_kinetics <- function(time, params) {
+  # set equations
+  # Definir las ecuaciones como funciones
+  equations <- list(
+    # "irreversible MM rate equation"
+    irrev = function(time, params) {
+      df <- with(params, {
+        data.frame(
+          time=time,
+          Substrate = Km*pracma::lambertWp(S0*exp((S0-Vmax*time)/Km)/Km), #Schnell-Mendoza eq
+          rate = Vmax*Substrate/(Km+Substrate) # MM equation
+        )})
+      return(df)},
+    # Competitive Inhibition
+    eq2 = function(time, params) { a * b * c },
+    eq3 = function(time, params) { a^2 + b^2 + c^2 }
+  )
+  # validate params names
   required_params <- unique(unlist(lapply(equations, function(eq) {
     names(formals(eq))
   })))
-
   if (!all(required_params %in% names(params))) {
     stop("Missing required parameters")
   }
@@ -40,5 +39,5 @@ simulate_kinetics <- function(from=0, to=1, n=10, params, equations=equations) {
   return(results)
 }
 # Evaluar las ecuaciones
-# results <- evaluate_equations(equations, params)
+# results <- simulate_kinetics(time, params)
 # print(results)
